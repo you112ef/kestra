2f8e530ab1c0ccb25b4547ba4e29cfae079fa735
Sandip Mandal <160670597+sandyp025@users.noreply.github.com>
2025-09-29 13:00:09 +0530

    chore(core): make sure kv listing is filterable (#11536)
    Closes https://github.com/kestra-io/kestra/issues/11413.
    
    Co-authored-by: Miloš Paunović <paun992@hotmail.com>

--- ui/src/components/kv/KVTable.vue
+++ ui/src/components/kv/KVTable.vue
@@ -232,7 +232,7 @@
                 return this.kvs?.filter(kv =>
                     !this.searchQuery ||
                     kv.key.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-                    kv.description.toLowerCase().includes(this.searchQuery.toLowerCase())
+                    kv.description?.toLowerCase().includes(this.searchQuery.toLowerCase())
                 );
             },
             kvModalTitle() {
